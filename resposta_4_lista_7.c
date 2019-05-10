7a525ad45426eedbf721e4ac0be7dff20e0920bd
Ariel-Henriques <48598433+Ariel-Henriques@users.noreply.github.com>
2019-05-10 20:28:05 -0300

    Create resposta_4_lista_7.c

---
+++ resposta_4_lista_7.c
@@ -0,0 +1,6 @@
+/* 
+    A função do exercicio 2 cria uma estrutura e copia os valores dela para a estrutura. Já a função do exercicio 3 usa o endereço da estrutura e a preenche.
+
+    Prefiro usar a abordagem do exercicio 3 porque, além de não precisar criar uma nova estrutura, ela "treina" o meu conhecimento de ponteiro.
+    
+    A função do exercicio 2 cria uma no estrutura, então exige um maior uso da memória e as velocidades das duas são contantes.
