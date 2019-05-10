a1753d04267333e92067e0c16be9a01b997d7fde
Ariel-Henriques <48598433+Ariel-Henriques@users.noreply.github.com>
2019-05-10 20:28:30 -0300

    Update resposta_4_lista_7.c

--- resposta_4_lista_7.c
+++ resposta_4_lista_7.c
@@ -3,4 +3,4 @@
 
     Prefiro usar a abordagem do exercicio 3 porque, além de não precisar criar uma nova estrutura, ela "treina" o meu conhecimento de ponteiro.
     
-    A função do exercicio 2 cria uma no estrutura, então exige um maior uso da memória e as velocidades das duas são contantes.
+    A função do exercicio 2 cria uma no estrutura, então exige um maior uso da memória e as velocidades das duas são contantes. */
